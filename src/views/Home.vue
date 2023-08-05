9741ca53de1d0ed9f1ecba668bfca2822143220e
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2023-08-05 17:45:54 +0300

    master router

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Header />
+    <About />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header/Header.vue";
+import About from "@/components/About/About.vue";
+export default {
+  name: "Home",
+  components: {
+    About,
+    Header,
+  },
+};
+</script>
